dc76a2ccc9a6377fccdf04a9d568a94828ac920b
SombrePigeon <contact@sombrepigeon.fr>
2026-08-14 14:34:58 +0200

    wip routing install only (no fetch)
    get all routes to cache, check composition and get static fragments
    remove fetch handler (see previous commit message)
    toDo clear logs await deletion in activation
    start fetching local compositions while global is loading, reorder param, custom namings ?

--- serviceWorker/routing.js
+++ serviceWorker/routing.js
@@ -8,9 +8,9 @@ const routes = [
 ];
 
 let urls;
-let urlsSet;
+let urlsSet = new Set();
 
-export function init(routes, config, version, composition, cacheName)
+export function install(routes, config, version, composition, baseUrl = import.meta.url, cacheName, namings)
 {
     _cacheName = cacheName ?? "alt-routing-routing";
     _cacheNameVersion = `${_cacheName}/${version}`;
@@ -21,7 +21,6 @@ export function init(routes, config, version, composition, cacheName)
         e =>
         {
             console.info(`${logPrefix} install version : ${version}`);
-            urlsSet = new Set();
 
             const install = async _ =>
                 {
@@ -36,26 +35,21 @@ export function init(routes, config, version, composition, cacheName)
                         console.debug(`${logPrefix} cache created : `, _cacheNameVersion);
                         const promises = [];
                         //get composition
-                        const compositionPath = composition ?? "../composition.json";
-                        await cache.add(compositionPath);
-                        const baseComposition = await cache.match(compositionPath, {cacheName: _cacheNameVersion });
+                        console.debug(`${logPrefix} récupération de la composition `);
+                        const compositionPath = composition ?? "../composition.json";//get from lib caches
+                        console.debug(`${logPrefix} baseurl : `, baseUrl);
+                        const compositionUrl = new URL(compositionPath, baseUrl);
+                        
+                        console.debug(`${logPrefix} cache composition : `, compositionUrl.href);
+                        await cache.add(compositionUrl);
+                        urlsSet.add(compositionUrl);
+                        const baseComposition = await cache.match(compositionUrl);
+
+                        console.debug(`${logPrefix} routes à mettre en cache`, routes);
                         for(const route of routes)
                         {
-                            let composition = baseComposition;
-                            if(config.route.localComposition)
-                            {
-                                //get composition
-                                //add to urlset
-                                //merge compositions
-                            }
-                            for(const fragment of fragments)
-                            {
-                                //if static, add
-                            }
-                            //list static fragments
-                            //add to cache and urlset
+                            promises.push(installRoute(route, config, compositionUrl, baseUrl));
                         }
-                        //old await cache.addAll(urls);
                         await Promise.all(promises);
                         console.debug(`${logPrefix} added to cache`);
                     }
@@ -64,7 +58,7 @@ export function init(routes, config, version, composition, cacheName)
             e.waitUntil(install());
         }
     )
-
+/*
     self.addEventListener("fetch",
         e =>
         {
@@ -77,7 +71,7 @@ export function init(routes, config, version, composition, cacheName)
             }
         }
     )
-
+*/
     self.addEventListener("activate",
         e =>
         {
@@ -103,3 +97,41 @@ export function init(routes, config, version, composition, cacheName)
     )
 
 }
+
+async function installRoute(path, config, baseComposition, baseUrl )
+{
+    const cache = await caches.open(_cacheNameVersion);
+    let composition =  await (await cache.match(baseComposition)).json();
+    const url = new URL(path, baseUrl);
+    console.debug(`${logPrefix} mise en cache de l'url : ${url}` );
+    console.debug(`${logPrefix} base composition : `, composition);
+
+    if(config.route.localComposition)
+    {
+        const compositionUrl = new URL("composition.json", url);
+        urlsSet.add(compositionUrl.href);
+        await cache.add(compositionUrl);
+        const localComposition = await (await cache.match(compositionUrl)).json();
+        console.debug(`${logPrefix} composition locale : `, localComposition);
+        //merge models
+        composition.models = {...composition.models, ...localComposition.models};
+        delete localComposition.models;
+        composition = {...composition, ...localComposition};
+    }
+    console.debug(`${logPrefix} composition mergée: `, composition);
+    const staticsFragmentsUrls = [];
+    for(const fragment of composition.fragments)
+    {
+        const model = composition.models[fragment];
+        console.debug(`${logPrefix} fragment : `,model);
+        if(model.static)
+        {
+            const fragmentUrl = new URL(fragment, url);
+            console.debug(`${logPrefix} add fragment url: `, fragmentUrl);
+            staticsFragmentsUrls.push(fragmentUrl);
+            urlsSet.add(fragmentUrl.href);
+        }
+    }
+    await cache.addAll(staticsFragmentsUrls)
+}
+
